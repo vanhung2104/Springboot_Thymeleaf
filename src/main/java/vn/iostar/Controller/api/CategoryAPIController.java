b3b09a70e737296d4b2cc90130b26a881318c40e
Hùng Nguyễn <vanhung211104@gmail.com>
2024-10-29 18:02:37 +0700

    Search and Pageable

--- src/main/java/vn/iostar/Controller/api/CategoryAPIController.java
+++ src/main/java/vn/iostar/Controller/api/CategoryAPIController.java
@@ -2,53 +2,53 @@ package vn.iostar.Controller.api;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import vn.iostar.entity.Category;
-import vn.iostar.model.Response;
 import vn.iostar.service.ICategoryService;
-import vn.iostar.service.IStorageService;
+
 @Controller
 @RequestMapping(path = "/api/category")
 public class CategoryAPIController {
     @Autowired
-    private ICategoryService categoryService;
-    @Autowired
-    IStorageService storageService;
+    ICategoryService categoryService;
 
     @GetMapping
     public String getAllCategory(Model model) {
         List<Category> list = categoryService.findAll();
-        model.addAttribute("list",list);
+        model.addAttribute("list", list);
         return "admin/category/list";
     }
+
     @GetMapping("/add")
-    public String add(Category category, ModelMap model){
+    public String add(Category category, ModelMap model) {
         model.addAttribute("category", category);
         return "admin/category/add";
     }
 
     @PostMapping("/saveadd")
-    public String saveadd(Category category, BindingResult result, Model model){
+    public String saveadd(Category category, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "admin/category/add";
         }
         categoryService.save(category);
         return "redirect:/api/category";
     }
+
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable("id") long id, Model model){
+    public String edit(@PathVariable("id") long id, Model model) {
         Category category = categoryService.findById(id)
                 .orElseThrow(() -> new RuntimeException("Not found"));
         model.addAttribute("category", category);
@@ -56,9 +56,9 @@ public class CategoryAPIController {
     }
 
     @PostMapping("/saveedit/{id}")
-    public String update(@PathVariable("id") long id,Category category, BindingResult result,
-                         Model model){
-        if (result.hasErrors()){
+    public String update(@PathVariable("id") long id, Category category, BindingResult result,
+                         Model model) {
+        if (result.hasErrors()) {
             return "admin/category/edit";
         }
         category.setCategoryId(id);
@@ -67,8 +67,45 @@ public class CategoryAPIController {
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") long id, Model model){
+    public String delete(@PathVariable("id") long id, Model model) {
         categoryService.deleteById(id);
         return "redirect:/api/category";
+    }
+
+    @RequestMapping("/search-page")
+    public String search(ModelMap model,
+                         @RequestParam(name = "name", required = false) String name,
+                         @RequestParam("page") Optional<Integer> page,
+                         @RequestParam("size") Optional<Integer> size) {
+        int count = (int)categoryService.count();
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(3);
+        Pageable pageable = PageRequest.of(currentPage - 1, pageSize, Sort.by("categoryName"));
+        Page<Category> resultPage = null;
+        if (StringUtils.hasText(name)) {
+            resultPage = categoryService.findByCategoryNameContaining(name, pageable);
+
+            model.addAttribute("name", name);
+        } else {
+            resultPage = categoryService.findAll(pageable);
+        }
+        int totalPages = resultPage.getTotalPages();
+
+        if (totalPages > 0) {
+            int start = Math.max(1, currentPage - 2);
+            int end = Math.min(currentPage + 2, totalPages);
+
+            if (totalPages > count) {
+                if (end == totalPages) start = end - count;
+                else if (start == 1) end = start + count;
+            }
+
+            List<Integer> pageNumbers = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+        model.addAttribute("categoryPage", resultPage);
+
+        return "admin/category/list_page";
     }
 }
